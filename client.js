1af2b8d1361d517faa465a0e3b54e248786d191b
Edlyn Shih <edlyn0418@gmail.com>
2022-08-06 22:28:04 -0400

    Export modules in Node.

---
+++ client.js
@@ -0,0 +1,19 @@
+// establishes a connection with the game server
+const net = require("net");
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541,
+  });
+
+  conn.on("data", () => {
+    console.log("you ded cuz you idled")
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+
+  return conn;
+};
+
+module.exports = {connect};
